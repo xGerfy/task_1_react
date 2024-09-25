4024063bbfb69314cc33e3a83710ceb33bf1f10f
Roman Ponomarev <xgerfy@gmail.com>
2024-09-25 20:45:59 +0400

    add PostList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
@@ -11,10 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 style={{ textAlign: "center" }}>Список постов</h1>
-      {posts.map((post) => (
-        <PostItem post={post} key={post.id} />
-      ))}
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
