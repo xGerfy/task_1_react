87f9c9461869ee2ee6c93947415ba96e5597cb88
Roman Ponomarev <xgerfy@gmail.com>
2024-09-25 22:18:04 +0400

    add sorting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MySelect from "./components/UI/select/MySelect";
 import "./styles/App.css";
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
     { id: 3, title: "Javascript 3", body: "Description 3" },
   ]);
 
+  const [selectedSort, setSelectedSort] = useState("");
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -18,10 +21,31 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сорторовка"
+          option={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
+      </div>
+      {posts.length ? (
+        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+      ) : (
+        <h1 style={{ textAlign: "center" }}>Посты не найдены!</h1>
+      )}
     </div>
   );
 }
---
+++ src/components/UI/select/MySelect.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MySelect({ option, defaultValue, value, onChange }) {
+  return (
+    <select value={value} onChange={(event) => onChange(event.target.value)}>
+      <option disabled value="">
+        {defaultValue}
+      </option>
+      {option.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+}
