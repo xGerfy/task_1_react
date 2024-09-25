cce606bef12ef773ccfe15442857afd3dd59b41b
Roman Ponomarev <xgerfy@gmail.com>
2024-09-25 20:15:20 +0400

    add PostItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
+
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description 2" },
+    { id: 3, title: "Javascript 3", body: "Description 3" },
+  ]);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 style={{ textAlign: "center" }}>Список постов</h1>
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PostItem(props) {
+  return (
+    <div>
+      <div className="post">
+        <div className="post__content">
+          <strong>
+            {props.post.id}. {props.post.title}
+          </strong>
+          <div>{props.post.body}</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+    </div>
+  );
+}
